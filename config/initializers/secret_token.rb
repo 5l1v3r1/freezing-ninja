1a9cb31e232d56706bc8dcbb97e9c77aee47ebfb
bradysullivan <Brady.Sullivan@iwsinc.com>
2013-03-08 16:51:09 -0800

    Initial rails build (attempt 2).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingNinja::Application.config.secret_token = '6226119a77103a9009896c94cdb451876a4d0c1d28a8ecdf5044cc915945ceedc0445b5719db0f717bc2f56444dd084f2dd63f73222e7f5ba0cc59e5c1b14999'
