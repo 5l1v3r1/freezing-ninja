15800d50783e5061db25c7a90a9878584a784fc6
bradysullivan <Brady.Sullivan@iwsinc.com>
2013-03-11 14:04:51 -0700

    Add tests for dynamic titles for About, Home, and Help.
    3 tests currently fail of 6. h1 tests pass but title tests do not.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,38 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do  
-    it "should have the content 'Freezing Ninja'" do
+    it "should have the h1 'Freezing Ninja'" do
       visit '/static_pages/home'
-      page.should have_content('Freezing Ninja')
+      page.should have_selector('h1', :text => 'Freezing Ninja')
     end 
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Freezing Ninja | Home")
+    end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Freezing Ninja | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content("About Us")
+      page.should have_selector('h1', :text => "About Us")
+    end
+    
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Freezing Ninja | About")
     end
   end
 end
